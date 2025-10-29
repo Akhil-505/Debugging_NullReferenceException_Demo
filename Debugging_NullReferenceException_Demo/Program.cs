7876c60f356a68bbe2b3b19e0aec360644907590
Narayanam.Kumar <Narayanam.Kumar@ENCDAPHYDLT0133>
2025-10-29 18:02:54 +0530

    Add project files.

---
+++ Debugging_NullReferenceException_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging_NullReferenceException_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> students = null;
+            List<string> students = new List<string>();
+            students.Add("Suraj");
+
+        }
+    }
+}
